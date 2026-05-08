161fb16e3600b3bfa19d8d2447410db11a818d8d
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-05-08 15:53:41 -0400

    Updated users.js route to work with mongoose

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function requireAuth(req, res, next) {
-    const header = req.headers.authorization; // Authorization header
+    const header = req.headers.authorization || ""; // Authorization header
     const token = header.startsWith("Bearer ") ? header.slice(7) : null; // Bearer token
 
     if(!token) return res.status(401).json({ message: "JWT token missing" }); // If no token, return 401 making route unaccessible
