aa4b04b5fb4817ea9d8ea9aa5d46468c8197402c
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-05-08 14:22:07 -0400

    Created accounts model

---
+++ server/models/accounts.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  lastUpdated: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Account", accountSchema);
