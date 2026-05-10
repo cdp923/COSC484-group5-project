768c5f3846d7fe70c91b303b85aeb272c5786eab
Denzel <120992039+jdenzel@users.noreply.github.com>
2026-05-09 20:46:56 -0400

    Updated fetch to have  headers to check for jwt token.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -11,24 +11,34 @@ export default function Dashboard() {
   const transactionsURL = `${baseURL}/transactions`;
   const budgetsURL = `${baseURL}/budgets`;
 
+  const token = localStorage.getItem("token");
+
   useEffect(() => {
     const getUser = async () => {
       try {
-        const response = await fetch(userURL);
-        if (!response.ok) {
+        const response = await fetch(userURL, {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+          },
+        });
+        if(response.status === 401) { // if token is invalid / unauthorized user then redirect to login page
+          localStorage.removeItem("token");
+          navigate("/login");
+          return;
+        }
+        if (!response.ok) { // if response is not ok then throw an error
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-
-        const data = await response.json();
+        const data = await response.json(); // if response is ok then set the user data to the state else throw an error
         setUsers(data);
       } catch (err) {
         setError(err.message);
-        console.error("fetch failed", err);
+        console.error("fetch failed", err); // if fetch fails then set the error to the state
       } finally {
-        setLoading(false);
+        setLoading(false); // if fetch is successful then set loading to false
       }
     };
-
     getUser();
   }, []);
 
