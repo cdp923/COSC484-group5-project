7426119d4ae59022a686d26a17e2d818f07899ac
cdp923 <158327739+cdp923@users.noreply.github.com>
2026-05-10 21:24:01 -0400

    Finished settings page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import "./Navbar.css";
 
 export default function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
     const navigate = useNavigate();
     const token = localStorage.getItem("token");
+    const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
     const handleLogout = () => {
+        setIsDropdownOpen(false);
         localStorage.removeItem("token");
         navigate("/login");
     }
-    const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            // If the menu is open and the user clicks something outside the ref
+            if (isDropdownOpen && dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsDropdownOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isDropdownOpen]);
     return (
         <div className='navbar'>
             <div className='navbar-links'>
@@ -20,14 +35,14 @@ export default function Navbar() {
             </div>
             <div className='navbar-menu'>
                 {token ? (
-                    <div className="dropdown-container">
-                    <button className="account-btn" onClick={toggleDropdown}>
-                        Account
-                    </button>
+                    <div className="dropdown-container" ref={dropdownRef}>
+                        <button className="account-btn" onClick={toggleDropdown}>
+                            Account
+                        </button>
                     
                     {isDropdownOpen && (
                         <div className="dropdown-menu">
-                            <Link to="/Home" onClick={() => setIsDropdownOpen(false)}>
+                            <Link to="/settings" onClick={() => setIsDropdownOpen(false)}>
                                 Settings
                             </Link>
                             <button className="dropdown-logout" onClick={handleLogout}>
